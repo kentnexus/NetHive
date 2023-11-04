49772b537dc7fade6af4bc2f48c4c5fb64eb30df
DhavalDandekar <dandekardhaval@gmail.com>
2023-11-04 14:29:03 -0700

    Asset Form and Table

---
+++ client/src/helpers/Controls.js
@@ -0,0 +1,12 @@
+import Input from "./Input";
+import Select from "./Select";
+import Datepicker from "./Datepicker";
+import Button from "./Button";
+const Controls = {
+  Input,
+  Select,
+  Datepicker,
+  Button,
+};
+
+export default Controls;
