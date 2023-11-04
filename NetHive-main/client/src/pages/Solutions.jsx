471875ccbf5202bb4dfc357b9e1faba48c2b7ac7
jonnalyn.sule@gmail.com <jonnalyn.sule@gmail.com>
2023-11-04 13:15:40 -0700

    added solutions tab

--- NetHive-main/client/src/pages/Solutions.jsx
+++ NetHive-main/client/src/pages/Solutions.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import MainPageLayout from "../components/MainPageLayout";
-import TabularExample from "../components/TabularExample";
 const bg = require("../images/bg_nethive.png");
-
+import RecoLists from "../components/RecoLists";
 
 
 const Solutions = () => {
@@ -16,8 +15,8 @@ const Solutions = () => {
       backgroundRepeat: "no-repeat",
       backgroundPosition: "center",
     }}>
-      {/* <h1>This is solutions content</h1> */}
-          <TabularExample/>
+      
+      <RecoLists/>
            </div>
     </MainPageLayout>
   );
