41edcbc000e9018d741f5ce946a3f791062a8e58
Tyrone Pastoril <tklpastoril@gmail.com>
2023-11-28 22:39:06 -0800

    Fixed bulk asset import based on serial number

--- server/controller/AssetsController.js
+++ server/controller/AssetsController.js
@@ -184,72 +184,131 @@ router.post("/bulk", async (req, res, next) => {
             stats.push(err)
           });
       } else if (!assetNumber) {
-        counterModel
-        .findOneAndUpdate(
-          { id: "autoAssetnNo" },
-          { $inc: { seq: 1 } },
-          { new: true }
-        )
-        .then((cd) => {
-          let seqId;
-          if (cd == null) {
-            const newVal = new counterModel({ id: "autoAssetnNo", seq: 1 });
-            newVal.save();
-            seqId =
-              "NH" +
-              cd.seq.toLocaleString("en-US", {
-                minimumIntegerDigits: 8,
-                useGrouping: false,
-              });
+        try {
+          const { serial_number} = asset;
+          const existingSerial = await Asset.findOne({
+            serial_number
+          })
+          if (!existingSerial){
+            counterModel
+            .findOneAndUpdate(
+              { id: "autoAssetnNo" },
+              { $inc: { seq: 1 } },
+              { new: true }
+            )
+            .then((cd) => {
+              let seqId;
+              if (cd == null) {
+                const newVal = new counterModel({ id: "autoAssetnNo", seq: 1 });
+                newVal.save();
+                seqId =
+                  "NH" +
+                  cd.seq.toLocaleString("en-US", {
+                    minimumIntegerDigits: 8,
+                    useGrouping: false,
+                  });
+              } else {
+                seqId =
+                  "NH" +
+                  cd.seq.toLocaleString("en-US", {
+                    minimumIntegerDigits: 8,
+                    useGrouping: false,
+                  });
+              }
+              const new_asset = new Asset({
+                assetNumber: seqId,              
+                customer_account: data[i].customer_account,
+                product: data[i].product,
+                asset_type: data[i].asset_type,
+                device_name: data[i].device_name,
+                manufacturer: data[i].manufacturer,
+                vendor: data[i].vendor,
+                model: data[i].model,
+                model_version: data[i].model_version,
+                serial_number: data[i].serial_number,
+                ip_address: data[i].ip_address,
+                snmp_community_string: data[i].snmp_community_string,
+                location: data[i].location,
+                owner_name: data[i].owner_name,
+                contracts_start_dt: data[i].contracts_start_dt,
+                contracts_end_dt: data[i].contracts_end_dt,
+                aggregated_to_: data[i].aggregated_to_,
+                status: data[i].status,
+                vendor_account_manager: data[i].vendor_account_manager,
+                contact_number: data[i].contact_number,
+                contact_email: data[i].contact_email,
+                website: data[i].website,
+                service_availed: data[i].service_availed,
+                cost: data[i].cost,
+                cost_frequency: data[i].cost_frequency,
+                tags: data[i].tags,
+                notes: data[i].notes,
+                modified_dt: date,
+                customer_name: data[i].customer_name,
+                created_by: data[i].created_by,
+                created_dt: data[i].created_dt,
+            });
+              new_asset
+                .save()
+          })
+            stats.push("Asset with serial number: "+ data[i].serial_number + " has been added.")
           } else {
-            seqId =
-              "NH" +
-              cd.seq.toLocaleString("en-US", {
-                minimumIntegerDigits: 8,
-                useGrouping: false,
+            const updateOps = {};
+            for (const [key, value] of Object.entries(asset)) {
+              updateOps[key] = value;
+            }
+            // console.log(updateOps);
+            Asset.updateOne(
+              {
+                serial_number: serial_number,
+              },
+              {
+                $set: {
+                  customer_account: updateOps["_doc"].customer_account,
+                  product: updateOps["_doc"].product,
+                  asset_type: updateOps["_doc"].asset_type,
+                  device_name: updateOps["_doc"].device_name,
+                  manufacturer: updateOps["_doc"].manufacturer,
+                  vendor: updateOps["_doc"].vendor,
+                  model: updateOps["_doc"].model,
+                  model_version: updateOps["_doc"].model_version,
+                  ip_address: updateOps["_doc"].ip_address,
+                  snmp_community_string: updateOps["_doc"].snmp_community_string,
+                  location: updateOps["_doc"].location,
+                  owner_name: updateOps["_doc"].owner_name,
+                  contracts_start_dt: updateOps["_doc"].contracts_start_dt,
+                  contracts_end_dt: updateOps["_doc"].contracts_end_dt,
+                  aggregated_to_: updateOps["_doc"].aggregated_to_,
+                  status: updateOps["_doc"].status,
+                  vendor_account_manager: updateOps["_doc"].vendor_account_manager,
+                  contact_number: updateOps["_doc"].contact_number,
+                  contact_email: updateOps["_doc"].contact_email,
+                  website: updateOps["_doc"].website,
+                  service_availed: updateOps["_doc"].service_availed,
+                  cost: updateOps["_doc"].cost,
+                  cost_frequency: updateOps["_doc"].cost_frequency,
+                  tags: updateOps["_doc"].tags,
+                  notes: updateOps["_doc"].notes,
+                  modified_dt: date,
+                  customer_name: updateOps["_doc"].customer_name,
+                  created_by: updateOps["_doc"].created_by,
+                },
+              }
+            )
+              .exec()
+              .then(
+                stats.push("Asset with serial number: "+ updateOps["_doc"].serial_number + " has been updated.")
+              )
+              .catch((err) => {
+                stats.push(err)
               });
           }
-        console.log(seqId);
-        const new_asset = new Asset({
-          assetNumber: seqId,
-          customer_account: data[i].customer_account,
-          product: data[i].product,
-          asset_type: data[i].asset_type,
-          device_name: data[i].device_name,
-          manufacturer: data[i].manufacturer,
-          vendor: data[i].vendor,
-          model: data[i].model,
-          model_version: data[i].model_version,
-          serial_number: data[i].serial_number,
-          ip_address: data[i].ip_address,
-          snmp_community_string: data[i].snmp_community_string,
-          location: data[i].location,
-          owner_name: data[i].owner_name,
-          contracts_start_dt: data[i].contracts_start_dt,
-          contracts_end_dt: data[i].contracts_end_dt,
-          aggregated_to_: data[i].aggregated_to_,
-          status: data[i].status,
-          vendor_account_manager: data[i].vendor_account_manager,
-          contact_number: data[i].contact_number,
-          contact_email: data[i].contact_email,
-          website: data[i].website,
-          service_availed: data[i].service_availed,
-          cost: data[i].cost,
-          cost_frequency: data[i].cost_frequency,
-          tags: data[i].tags,
-          notes: data[i].notes,
-          modified_dt: date,
-          customer_name: data[i].customer_name,
-          created_by: data[i].created_by,
-          created_dt: data[i].created_dt,
-        });
-        new_asset
-          .save()
-      })
-        stats.push(data[i].seqId + " has been added.")
+        } catch (err) {
+          stats.push(err)
+        }
       } else {
-        asset.save(data[i])
-        stats.push(data[i].assetNumber + " has been added.")
+        asset.save(asset)
+        stats.push(asset.assetNumber + " has been added.")
       }
     } catch (err) {
       stats.push(err)
