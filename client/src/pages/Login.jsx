99fc8c30fc6987cfb2c28a2169779eaa40b865d4
Tyrone Pastoril <tklpastoril@gmail.com>
2023-11-30 23:22:32 -0800

    Fixed login api url

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/login",
+        "/api/login",
         {
           ...inputValue,
         },
